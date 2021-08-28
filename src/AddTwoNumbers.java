39b753f458cddb8fadcbe6d757bd335de2dd3192
mliu <mliu@staff.compass.edu.au>
2021-08-28 12:08:03 +1000

    2 - add two numbers

---
+++ src/AddTwoNumbers.java
@@ -0,0 +1,50 @@
+import java.util.List;
+
+public class AddTwoNumbers {
+    public void Run() {
+        ListNode node1 = new ListNode(2);
+        ListNode node2 = new ListNode(4);
+        ListNode node3 = new ListNode(3);
+
+        ListNode node4 = new ListNode(5);
+        ListNode node5 = new ListNode(6);
+        ListNode node6 = new ListNode(4);
+
+        node1.next = node2;
+        node2.next = node3;
+
+        node4.next = node5;
+        node5.next = node6;
+
+        ListNode result = addTwoNumbers(node1, node4);
+    }
+
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null, tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+        return head;
+    }
+
+}
---
+++ src/RunSolution.java
@@ -0,0 +1,6 @@
+public class RunSolution {
+    public static void main(String[] args){
+        AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
+        addTwoNumbers.Run();
+    }
+}
