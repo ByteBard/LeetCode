35917dd47ed39d8607fcb33dd942b5837a913d29
liumingvic@gmail.com <liumingvic@gmail.com>
2021-08-28 12:34:25 +1000

    test

--- src/RunSolution.java
+++ src/RunSolution.java
@@ -1,6 +1,6 @@
 public class RunSolution {
     public static void main(String[] args){
         AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
-        addTwoNumbers.Run();
+        addTwoNumbers.Run(); 
     }
 }
