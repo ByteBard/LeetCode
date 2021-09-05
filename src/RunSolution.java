2ef3caaca0669b3018f01a3551ef0572e78d862f
liumingvic@gmail.com <liumingvic@gmail.com>
2021-09-05 13:47:50 +1000

    239 SlidingWindowMaximum

--- src/RunSolution.java
+++ src/RunSolution.java
@@ -1,6 +1,6 @@
 public class RunSolution {
     public static void main(String[] args){
-        MinimumSizeSubArraySum x = new MinimumSizeSubArraySum();
+        SlidingWindowMaximum x = new SlidingWindowMaximum();
         x.run();
     }
 }
