cb247c5996b2473a8ae8214c8e9a012ea66f7f97
mliu <liumingvic@gmail.com>
2021-10-29 00:08:03 +1100

    7

---
+++ ReverseInteger.java
@@ -0,0 +1,47 @@
+package LeetCode;
+
+public class ReverseInteger {
+    //better one
+    public int reverse01(int x) {
+        int rev = 0;
+        while (x != 0) {
+            if (rev < Integer.MIN_VALUE / 10 || rev > Integer.MAX_VALUE / 10) {
+                return 0;
+            }
+            int digit = x % 10;
+            x /= 10;
+            rev = rev * 10 + digit;
+        }
+        return rev;
+    }
+
+    public int reverse02(int x) {
+        int res = 0;
+        while(x!=0) {
+            //每次取末尾数字
+            int tmp = x%10;
+            //判断是否 大于 最大32位整数
+            if (res>214748364 || (res==214748364 && tmp>7)) {
+                return 0;
+            }
+            //判断是否 小于 最小32位整数
+            if (res<-214748364 || (res==-214748364 && tmp<-8)) {
+                return 0;
+            }
+            res = res*10 + tmp;
+            x /= 10;
+        }
+        return res;
+    }
+
+// 以下链接附有相似题。。。
+//    作者：wang_ni_ma
+//    链接：https://leetcode-cn.com/problems/reverse-integer/solution/tu-jie-7-zheng-shu-fan-zhuan-by-wang_ni_ma/
+//    来源：力扣（LeetCode）
+//    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+//
+//    作者：LeetCode-Solution
+//    链接：https://leetcode-cn.com/problems/reverse-integer/solution/zheng-shu-fan-zhuan-by-leetcode-solution-bccn/
+//    来源：力扣（LeetCode）
+//    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+}
