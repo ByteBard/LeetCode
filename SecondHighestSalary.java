4ffe18d9ce75d362517400c608b8701a5b0eded1
ByteBard <liumingvic@gmail.com>
2021-10-31 22:52:47 +1100

    176

---
+++ SecondHighestSalary.java
@@ -0,0 +1,16 @@
+package LeetCode;
+
+public class SecondHighestSalary {
+//    SELECT
+//    IFNULL(
+//      (SELECT DISTINCT Salary
+//            FROM Employee
+//            ORDER BY Salary DESC
+//            LIMIT 1 OFFSET 1),
+//    NULL) AS SecondHighestSalary
+//
+//    作者：LeetCode
+//    链接：https://leetcode-cn.com/problems/second-highest-salary/solution/di-er-gao-de-xin-shui-by-leetcode/
+//    来源：力扣（LeetCode）
+//    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+}
