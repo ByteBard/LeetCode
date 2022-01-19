e1dac880cd82ae42f65b1e83ab10f3fab083896d
Ming Liu <mliu@compass.education>
2022-01-19 16:04:49 +1100

    567 update

--- PermutationInString.java
+++ PermutationInString.java
@@ -5,6 +5,31 @@ import java.util.HashSet;
 
 public class PermutationInString {
 
+    /*
+    567. 字符串的排列
+给你两个字符串 s1 和 s2 ，写一个函数来判断 s2 是否包含 s1 的排列。如果是，返回 true ；否则，返回 false 。
+
+换句话说，s1 的排列之一是 s2 的 子串 。
+
+
+
+示例 1：
+
+输入：s1 = "ab" s2 = "eidbaooo"
+输出：true
+解释：s2 包含 s1 的排列之一 ("ba").
+示例 2：
+
+输入：s1= "ab" s2 = "eidboaoo"
+输出：false
+
+
+提示：
+
+1 <= s1.length, s2.length <= 104
+s1 和 s2 仅包含小写字母
+     */
+
     public void run() {
         //String s1 = "ab", s2 = "eidboaoo";
         String s1 = "abb", s2 = "aaacccbb";
@@ -14,6 +39,7 @@ public class PermutationInString {
 
     }
 
+    //66.87%, 38.41%
     public boolean checkInclusion_mliu(String s1, String s2) {
         int left = 0;
         int right = 0;
@@ -54,6 +80,7 @@ public class PermutationInString {
         return false;
     }
 
+    //66.87%, 23.00%
     public boolean checkInclusion_mliu01(String s1, String s2) {
         int n = s1.length(), m = s2.length();
         if (n > m) {
@@ -83,6 +110,7 @@ public class PermutationInString {
         return false;
     }
 
+    //76.32%, 38.21%
     public boolean checkInclusion_offical(String s1, String s2) {
         int n = s1.length(), m = s2.length();
         if (n > m) {
@@ -113,6 +141,7 @@ public class PermutationInString {
 //    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
     //imitate 76 (same idea)
+    //57.29%, 18.9%
     public boolean checkInclusion_offical_01(String s1, String s2) {
         char[] pattern = s1.toCharArray();
         char[] text = s2.toCharArray();
