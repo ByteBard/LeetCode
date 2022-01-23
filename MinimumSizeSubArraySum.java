819a49c605238a5b1dcfa5731b68a32469afea79
Ming Liu <mliu@compass.education>
2022-01-23 17:32:46 +1100

    209 update

--- MinimumSizeSubArraySum.java
+++ MinimumSizeSubArraySum.java
@@ -42,40 +42,6 @@ public class MinimumSizeSubArraySum {
         System.out.println(minSubArrayLen_209(target, nums));
     }
 
-    //17%, 11%
-    public int minSubArrayLen_mliu(int target, int[] nums) {
-
-        int left = 0;
-        int right = 0;
-        int sum = 0;
-        int size = nums.length;
-        int minLength = 0;
-
-        while (right < size) {
-            int currentNum = nums[right];
-            if (currentNum >= target) return 1;
-            sum += currentNum;
-            if (sum >= target) {
-                if (minLength == 0) {
-                    minLength = right - left + 1;
-                } else {
-                    minLength = Math.min(minLength, right - left + 1);
-                }
-            }
-
-            while (left <= right && sum >= target) {
-                minLength = Math.min(minLength, right - left + 1);
-                int currentNumAtLeft = nums[left];
-                left++;
-                sum -= currentNumAtLeft;
-            }
-
-            right++;
-        }
-
-        return minLength;
-    }
-
     //50%, 28%
     public int minSubArrayLen_209(int s, int[] nums) {
         int n = nums.length;
