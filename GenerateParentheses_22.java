62b2e3ddaacb3c41489b8da4a440f469b60d0bbd
ByteBard <liumingvic@gmail.com>
2022-02-21 08:26:59 +1100

    'update'

--- GenerateParentheses_22.java
+++ GenerateParentheses_22.java
@@ -5,8 +5,27 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-public class GenerateParenthesis {
+public class GenerateParentheses_22 {
+/*
+22. 括号生成
+数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
 
+
+
+示例 1：
+
+输入：n = 3
+输出：["((()))","(()())","(())()","()(())","()()()"]
+示例 2：
+
+输入：n = 1
+输出：["()"]
+
+
+提示：
+
+1 <= n <= 8
+ */
     public void run() {
         int x = 3;
         List<String> result = generateParenthesisFormal(x);
--- SubstringWithConcatenationOfAllWords.java
+++ SubstringWithConcatenationOfAllWords.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
-public class FindSubstring {
+public class SubstringWithConcatenationOfAllWords {
 /*
 30. 串联所有单词的子串
 给定一个字符串 s 和一些 长度相同 的单词 words 。找出 s 中恰好可以由 words 中所有单词串联形成的子串的起始位置。
