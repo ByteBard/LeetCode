731d11ba2c4b50390b01e796958edeb1faf24935
Ming Liu <mliu@compass.education>
2022-08-07 00:04:45 +1000

    253

--- MeetingRoomsII_253.java
+++ MeetingRoomsII_253.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
-public class MeetingRoomsII {
+public class MeetingRoomsII_253 {
 
     public int minMeetingRooms(int[][] intervals) {
 
