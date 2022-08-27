f1cfb8b3e50459d11f25463bf4a1118e0033aeb3
Ming Liu <liumingvic@gmail.com>
2022-08-27 19:02:08 +0800

    test

--- RunSolution.java
+++ RunSolution.java
@@ -3,6 +3,6 @@ package LeetCode;
 public class RunSolution {
     public static void main(String[] args){
         BinaryTreeCameras_968  x  = new BinaryTreeCameras_968();
-        x.run();
+        x.run() ;
     }
 }
