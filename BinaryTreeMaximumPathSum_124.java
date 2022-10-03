c25090ee0ccd4f8221346766b8fb217e3b908869
Ming Liu <mliu@compass.education>
2022-10-03 13:48:44 +1100

    124

--- BinaryTreeMaximumPathSum_124.java
+++ BinaryTreeMaximumPathSum_124.java
@@ -12,28 +12,21 @@ public class BinaryTreeMaximumPathSum_124 {
         }
     }
 
-    int max_sum = Integer.MIN_VALUE;
+    int maxNum = Integer.MIN_VALUE;
 
-    public int max_gain(TreeNode node) {
-        if (node == null) return 0;
-
-        // max sum on the left and right sub-trees of node
-        int left_gain = Math.max(max_gain(node.left), 0);
-        int right_gain = Math.max(max_gain(node.right), 0);
-
-        // the price to start a new path where `node` is a highest node
-        int price_newpath = node.val + left_gain + right_gain;
-
-        // update max_sum if it's better to start a new path
-        max_sum = Math.max(max_sum, price_newpath);
-
-        // for recursion :
-        // return the max gain if continue the same path
-        return node.val + Math.max(left_gain, right_gain);
+    public int get_max(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int maxLeft = Math.max(get_max(node.left), 0);
+        int maxRight = Math.max(get_max(node.right), 0);
+        int maxPathVal = maxLeft + maxRight + node.val;
+        maxNum = Math.max(maxNum, maxPathVal);
+        return node.val + Math.max(maxLeft, maxRight);
     }
 
     public int maxPathSum(TreeNode root) {
-        max_gain(root);
-        return max_sum;
+        get_max(root);
+        return maxNum;
     }
 }
