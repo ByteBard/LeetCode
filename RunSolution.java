38c1b687692af5ab1080ae3ae447b677326867d5
ByteBard <liumingvic@gmail.com>
2022-12-27 00:41:13 +1100

    '10'

--- RunSolution.java
+++ RunSolution.java
@@ -2,18 +2,13 @@ package LeetCode;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class RunSolution {
     public static void main(String[] args) {
-        EmployeeFreeTime_759 x = new EmployeeFreeTime_759();
-        x.run();
-//        int[] x = new int[]{1, 2, 1, 0, 2, 1, 0, 1};
-//        System.out.println(minTapsFormal(7, x));
+
 
     }
-
-
-
 }
